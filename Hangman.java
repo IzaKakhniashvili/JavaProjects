953d6c7f947a9ad7c29305d1f4d8225801ed7b63
S <s>
2023-12-16 22:14:58 +0400

    {"types":"C","runs":7}

--- Hangman.java
+++ Hangman.java
@@ -14,17 +14,30 @@ import java.awt.*;
 public class Hangman extends ConsoleProgram {
 	private HangmanLexicon word;
 	private String RandomWord;
+	private int ATTEMPTS;
+	private String guessedWord;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
-    public void run() {
+    
+	
+	public void run() {
     	println("Welcome to Hangman!");
     	word = new HangmanLexicon();
     	selectRandomWord();
     	println(RandomWord);
+    	GuessedWord();
+    	println(guessedWord);
 		/* You fill this in */
 	}
 	public void selectRandomWord(){
 		int index = rgen.nextInt(0,10);  //aqq reinji aris shesacvleli da ar damaviwkdes!!
 		RandomWord = word.getWord(index);
+	}
+	
+	private void GuessedWord(){
+		guessedWord = " ";
+		for(int i = 0; i < RandomWord.length(); i++){
+			guessedWord += "_" ; 
+		}
 	}
 
 }
