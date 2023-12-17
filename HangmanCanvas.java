5d0e5f22bb57ab763af6d5c70ccf16341fcfdd4a
S <s>
2023-12-17 21:56:49 +0400

    {"types":"C","runs":3}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -41,7 +41,7 @@ public class HangmanCanvas extends GCanvas {
 		incorrect.setLocation(x, y);
 		incorrect.setFont("Font.ITALIC-20");
 		add(incorrect);
-		VX+=12;
+		VX+=15;
 	}
 	public void Gallows(){
 		GLine scaffold = new GLine(XOFFSET, YOFFSET, XOFFSET, YOFFSET + SCAFFOLD_HEIGHT);
