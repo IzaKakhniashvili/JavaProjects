f5949baa5e709959dc754965c40875a8c9506784
S <s>
2023-12-17 20:16:41 +0400

    {"types":"C","runs":84}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -21,7 +21,7 @@ public class HangmanCanvas extends GCanvas {
  */
 	public void displayWord(String word) {
 		int x = XOFFSET;
-		int y = SCAFFOLD_HEIGHT + YOFFSET + 5;
+		int y = SCAFFOLD_HEIGHT + YOFFSET + 15;
 		GLabel guessedWord = new GLabel(word);
 		guessedWord.setLocation(x, y);
 		add(guessedWord);
